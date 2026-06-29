5772a6e1e2a2301a4c3ed0eb4c524e206153e8cb
AstelCode <frankparedesalpi@gmail.com>
2026-06-28 21:38:56 -0500

    add galary product details

---
+++ AllasiFronted/Components/Pages/Admin/ReporteProducto.razor.js
@@ -0,0 +1,10 @@
+﻿window.descargarArchivo = (nombreArchivo, tipoContenido, base64) => {
+    const link = document.createElement("a");
+
+    link.download = nombreArchivo;
+    link.href = `data:${tipoContenido};base64,${base64}`;
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
