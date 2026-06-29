218f08b98437bcd1ff7960830706984aa48e3602
mauricioj-gordillop <mauricioj.gordillop@pucp.edu.pe>
2026-06-29 01:30:14 -0500

    29/06/2026 01:30 Crear cuenta

--- AllasiFronted/Services/ConfirmDialogService.cs
+++ AllasiFronted/Services/ConfirmDialogService.cs
@@ -3,15 +3,23 @@ using System.Threading.Tasks;
 
 namespace Progra3_Frontend.Services
 {
+    public enum DialogType
+    {
+        Danger,
+        Info,
+        Success,
+        Warning
+    }
+
     public class ConfirmDialogService
     {
-        public event Func<string, string, Task<bool>>? OnShow;
+        public event Func<string, string, DialogType, Task<bool>>? OnShow;
 
-        public async Task<bool> ConfirmAsync(string title, string message)
+        public async Task<bool> ConfirmAsync(string title, string message, DialogType type = DialogType.Danger)
         {
             if (OnShow != null)
             {
-                return await OnShow.Invoke(title, message);
+                return await OnShow.Invoke(title, message, type);
             }
             return false;
         }
